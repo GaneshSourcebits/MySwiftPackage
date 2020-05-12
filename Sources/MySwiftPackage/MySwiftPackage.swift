996fed31a4b3ba2c1dc2e8daa7cfaf1f6ec0b5e1
Ganesh Kumar <ganesh.kumar@sourcebits.com>
2020-05-12 14:59:46 +0530

    Added sample function to test

--- Sources/MySwiftPackage/MySwiftPackage.swift
+++ Sources/MySwiftPackage/MySwiftPackage.swift
@@ -1,3 +1,14 @@
 struct MySwiftPackage {
     var text = "Hello, World!"
+    
+    func printHelloWorld() {
+        print("Hello World!")
+    }
+}
+
+class MySwiftPackage2 {
+    
+    func printHelloWorld() {
+        print("Hello World!")
+    }
 }
